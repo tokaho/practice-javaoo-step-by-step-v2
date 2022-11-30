f1cab9b123e02ddbcc554f9e3ee5562099bcb03e
tokaho <tokaho2000@gmail.com>
2022-11-30 17:13:25 +0800

    [feat] implement Person

--- src/main/java/ooss/Person.java
+++ src/main/java/ooss/Person.java
@@ -1,4 +1,33 @@
 package ooss;
 
 public class Person {
+    private final int id;
+    private final String name;
+    private final int age;
+
+    public Person(int id, String name, int age){
+        this.id=id;
+        this.name=name;
+        this.age=age;
+    }
+
+    public String introduce(){
+        return String.format("My name is %s. I am %d years old.",this.name,this.age);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
 }
+
--- src/test/java/ooss/step1/PersonTest.java
+++ src/test/java/ooss/step1/PersonTest.java
@@ -7,36 +7,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PersonTest {
 
-//    @Test
-//    public void should_return_message_with_name_and_age_when_introduce_given_person() {
-//        Person tom = new Person(1, "Tom", 18);
-//        String introduce = tom.introduce();
-//
-//        assertEquals("My name is Tom. I am 18 years old.", introduce);
-//    }
-//
-//    @Test
-//    public void should_return_message_with_name_and_age_when_introduce_given_another_person() {
-//        Person jim = new Person(2, "Jim", 19);
-//        String introduce = jim.introduce();
-//
-//        assertEquals("My name is Jim. I am 19 years old.", introduce);
-//    }
-//
-//    @Test
-//    public void should_return_true_when_equals_given_two_people_with_same_id() {
-//        Person person1 = new Person(1, "Tom", 18);
-//        Person person2 = new Person(1, "Tommy", 18);
-//
-//        assertEquals(person1, person2);
-//    }
-//
-//    @Test
-//    public void should_return_false_when_equals_given_two_people_with_different_id() {
-//        Person person1 = new Person(1, "Tom", 18);
-//        Person person2 = new Person(2, "Tom", 18);
-//
-//        assertNotEquals(person1, person2);
-//    }
+    @Test
+    public void should_return_message_with_name_and_age_when_introduce_given_person() {
+        Person tom = new Person(1, "Tom", 18);
+        String introduce = tom.introduce();
+
+        assertEquals("My name is Tom. I am 18 years old.", introduce);
+    }
+
+    @Test
+    public void should_return_message_with_name_and_age_when_introduce_given_another_person() {
+        Person jim = new Person(2, "Jim", 19);
+        String introduce = jim.introduce();
+
+        assertEquals("My name is Jim. I am 19 years old.", introduce);
+    }
+
+    @Test
+    public void should_return_true_when_equals_given_two_people_with_same_id() {
+        Person person1 = new Person(1, "Tom", 18);
+        Person person2 = new Person(1, "Tommy", 18);
+
+        assertEquals(person1, person2);
+    }
+
+    @Test
+    public void should_return_false_when_equals_given_two_people_with_different_id() {
+        Person person1 = new Person(1, "Tom", 18);
+        Person person2 = new Person(2, "Tom", 18);
+
+        assertNotEquals(person1, person2);
+    }
 
 }
