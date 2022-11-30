f4ae277bb119f1a93a971a0cb15bf192665cfaf2
tokaho <tokaho2000@gmail.com>
2022-11-30 17:54:29 +0800

    [feat] (step4) implement (assignTo, belongsTo, isTeaching) in Teacher

--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -1,11 +1,38 @@
 package ooss;
 
+import java.util.HashSet;
+import java.util.stream.Collectors;
+
 public class Teacher extends Person{
+    private HashSet<Klass> teachingKlasses =new HashSet<Klass>();
     public Teacher(int id, String name, int age){
         super(id,name,age);
     }
 
     public String introduce(){
-        return super.introduce()+" I am a teacher.";
+        return super.introduce()+
+                " I am a teacher."+
+                (!this.teachingKlasses.isEmpty()?
+                        //true
+                        String.format(" I teach Class %s.",
+                                this.teachingKlasses.stream()
+                                        .map(k->String.valueOf(k.getNumber()))
+                                        .collect(Collectors.joining(", "))
+                        ):
+                        //else
+                        ""
+                );
+    }
+    public void assignTo(Klass klass){
+        this.teachingKlasses.add(klass);
+    }
+    public boolean belongsTo(Klass klass){
+        return this.teachingKlasses.contains(klass);
+    }
+
+    public boolean isTeaching(Student student){
+        return this.teachingKlasses.stream()
+                .map(klass-> student.isIn(klass))
+                .collect(Collectors.reducing(Boolean.FALSE, Boolean::logicalOr));
     }
 }
--- src/test/java/ooss/step3/KlassTest.java
+++ src/test/java/ooss/step3/KlassTest.java
@@ -8,20 +8,20 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class KlassTest {
 
-//    @Test
-//    public void should_return_true_when_equals_given_two_classes_with_same_id() {
-//        Klass klass1 = new Klass(1);
-//        Klass klass2 = new Klass(1);
-//
-//        assertEquals(klass1, klass2);
-//    }
-//
-//    @Test
-//    public void should_return_false_when_equals_given_two_classes_with_different_id() {
-//        Klass klass1 = new Klass(1);
-//        Klass klass2 = new Klass(2);
-//
-//        assertNotEquals(klass1, klass2);
-//    }
+    @Test
+    public void should_return_true_when_equals_given_two_classes_with_same_id() {
+        Klass klass1 = new Klass(1);
+        Klass klass2 = new Klass(1);
+
+        assertEquals(klass1, klass2);
+    }
+
+    @Test
+    public void should_return_false_when_equals_given_two_classes_with_different_id() {
+        Klass klass1 = new Klass(1);
+        Klass klass2 = new Klass(2);
+
+        assertNotEquals(klass1, klass2);
+    }
 
 }
